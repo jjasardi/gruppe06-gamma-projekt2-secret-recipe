c6b7dbc086c34789f2d2e55acace9e4de45ec896
Yohan Unver <yohan.unver@gmail.com>
2022-04-25 09:39:36 +0200

    created recipe class #2

---
+++ app/src/main/java/ch/zhaw/pm2/secretrecipe/Recipe.java
@@ -0,0 +1,52 @@
+package ch.zhaw.pm2.secretrecipe;
+
+/**
+ *
+ */
+public class Recipe {
+    private String name;
+    private String ingredients;
+    private String description;
+    private User owner;
+
+    /**
+     * @param name
+     * @param ingredients
+     * @param description
+     * @param owner
+     */
+    public Recipe(String name, String ingredients, String description, User owner) {
+        this.name = name;
+        this.ingredients = ingredients;
+        this.description = description;
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+}
