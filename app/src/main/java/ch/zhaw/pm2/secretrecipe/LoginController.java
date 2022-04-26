37146862fc58b6e66058d8a4ed62bbf2fa663aab
jasarard <jasarard@students.zhaw.ch>
2022-04-26 17:05:01 +0200

    started with implementation of Controller for Login #6

---
+++ app/src/main/java/ch/zhaw/pm2/secretrecipe/LoginController.java
@@ -0,0 +1,31 @@
+package ch.zhaw.pm2.secretrecipe;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button switchToRegisterButton;
+
+    public void initialize() {
+
+    }
+
+    @FXML
+    private void loginUser(){
+        
+    }
+
+    @FXML
+    private void switchToRegisterButton() {
+        
+    }
+}
