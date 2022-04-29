de6f549c38656fc3cb1f35437f433f16493888c5
gabricyr <gabricyr@students.zhaw.ch>
2022-04-29 13:40:58 +0200

    added logo to rescource folder, edited logon controller to a first version, created a exception class for invalid user entry in login

--- app/src/main/java/ch/zhaw/pm2/secretrecipe/LoginController.java
+++ app/src/main/java/ch/zhaw/pm2/secretrecipe/LoginController.java
@@ -1,40 +1,50 @@
 package ch.zhaw.pm2.secretrecipe;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
-public class LoginController {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class LoginController implements ScreenController{
+    private HashMap<String, Parent> screens = new HashMap<>();
+    private List<User> userList = new ArrayList<>();
+    User user;
+
     @FXML
     private TextField usernameField;
     @FXML
     private PasswordField passwordField;
     @FXML
-    private Button loginButton;
-    @FXML
-    private Button switchToRegisterButton;
-
-    public void initialize() {
-
-    }
+    private AnchorPane root;
 
     @FXML
-    private void loginUser(){
-        
-    }
-
-    @FXML
-    private void switchToRegisterButton() {
-        
-    }
-
-    public User getUserMatchingCredentials(String username, String password) {
-        for (User user : userList) {
-            if (username.equals(user.getUsername()) && password.equals(user.getPassword())) {
-                return user;
+    void loginUser(ActionEvent event) {
+        try {
+            String userName = usernameField.getText();
+            String password = passwordField.getText();
+            if(!userList.contains(user)) {
+                throw new InvalidUserEntry("Invalid user entry!");
             }
+        } catch (InvalidUserEntry exception) {
+
         }
-        return null;
     }
+
+    @FXML
+    void switchToRegisterView(ActionEvent event) {
+        root.getScene().setRoot(screens.get(App.REGISTRATION));
+    }
+
+    @Override
+    public void setScreenList(HashMap<String, Parent> screens) {
+        this.screens = screens;
+    }
+
 }
