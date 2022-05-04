d1d4992a5634755151e86c26b1841b301cc40bda
gabricyr <gabricyr@students.zhaw.ch>
2022-05-04 18:15:01 +0200

    implemented comments from yohan on pull request #3

--- app/src/main/java/ch/zhaw/pm2/secretrecipe/DetailController.java
+++ app/src/main/java/ch/zhaw/pm2/secretrecipe/DetailController.java
@@ -2,14 +2,10 @@ package ch.zhaw.pm2.secretrecipe;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Menu;
+import javafx.scene.Parent;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.Parent;
 
 import java.util.HashMap;
 import java.util.List;
