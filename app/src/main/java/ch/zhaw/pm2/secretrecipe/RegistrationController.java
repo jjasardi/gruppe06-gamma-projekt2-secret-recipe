3f57727888591fce7bb9427d0597cd7b9b8ba588
jasarard <jasarard@students.zhaw.ch>
2022-05-06 11:46:31 +0200

    registrationController completed #6

--- app/src/main/java/ch/zhaw/pm2/secretrecipe/RegistrationController.java
+++ app/src/main/java/ch/zhaw/pm2/secretrecipe/RegistrationController.java
@@ -7,15 +7,13 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
 
 import java.util.HashMap;
 
 public class RegistrationController implements ScreenController {
     private HashMap<String, Parent> screens = new HashMap<>();
+    private DataManager dataManager;
+    private Session session;
 
     @FXML
     private TextField fistnameField;
@@ -33,18 +31,41 @@ public class RegistrationController implements ScreenController {
     private TextField usernameField;
 
     @FXML
-    void backToLoginView(ActionEvent event) {
+    public void initialize() {
+        dataManager = DataManager.getInstance();
+        session = Session.getInstance();
+    }
+
+    @FXML
+    private void backToLoginView(ActionEvent event) {
         root.getScene().setRoot(screens.get(App.LOGIN));
     }
 
     @FXML
-    void registerUser(ActionEvent event) {
+    private void registerUser(ActionEvent event) {
         String username = usernameField.getText();
         String firstname = fistnameField.getText();
         String surname = surnameField.getText();
         String password = passwordField.getText();
+        if (!isUsernameTaken(username)) {
+            User newUser = new User(firstname, surname, username, password)
+            dataManager.addUser(newUser);
+            session.setLoggedInUser(newUser);
+            goToStartView();
+        }
+    }
 
-        User user = new User(firstname, surname, username, password);
+    private void goToStartView() {
+        root.getScene().setRoot(screens.get(App.START));
+    }
+
+    private boolean isUsernameTaken(String username) {
+        for (User registeredUser : dataManager.getUserList()) {
+            if (username.equals(registeredUser.getUsername())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
