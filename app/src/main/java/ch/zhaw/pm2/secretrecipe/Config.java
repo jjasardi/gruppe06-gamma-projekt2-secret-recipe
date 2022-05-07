3a001bab314db07a79b16d038ed079cc4e008b32
Yohan Unver <yohan.unver@gmail.com>
2022-05-07 13:18:10 +0200

    changed fxml file names #20

--- app/src/main/java/ch/zhaw/pm2/secretrecipe/Config.java
+++ app/src/main/java/ch/zhaw/pm2/secretrecipe/Config.java
@@ -6,13 +6,13 @@ package ch.zhaw.pm2.secretrecipe;
 public class Config {
 
     public static final String START = "Start";
-    public static final String START_FILE = "/start.fxml";
+    public static final String START_FILE = "/Start.fxml";
     public static final String NEWRECIPE = "NewRecipe";
-    public static final String NEWRECIPE_FILE = "/newRecipe.fxml";
+    public static final String NEWRECIPE_FILE = "/NewRecipe.fxml";
     public static final String LOGIN = "Login";
-    public static final String LOGIN_FILE = "/login.fxml";
+    public static final String LOGIN_FILE = "/Login.fxml";
     public static final String REGISTRATION = "Registration";
-    public static final String REGISTRATION_FILE = "/registration.fxml";
+    public static final String REGISTRATION_FILE = "/Registration.fxml";
     public static final String DETAIL = "Detail";
-    public static final String DETAIL_FILE = "/detail.fxml";
+    public static final String DETAIL_FILE = "/Detail.fxml";
 }
