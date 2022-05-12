d05355b24cf9bc9f88437458e615dcca781af241
yohan <yohan.unver@gmail.com>
2022-05-12 23:07:25 +0200

    implemented initialize method with listener on boolean loggedIn property #24

--- app/src/main/java/ch/zhaw/pm2/secretrecipe/ui/StartController.java
+++ app/src/main/java/ch/zhaw/pm2/secretrecipe/ui/StartController.java
@@ -1,14 +1,19 @@
 package ch.zhaw.pm2.secretrecipe.ui;
 
 import ch.zhaw.pm2.secretrecipe.Config;
+import ch.zhaw.pm2.secretrecipe.model.Recipe;
+import ch.zhaw.pm2.secretrecipe.model.Session;
+import ch.zhaw.pm2.secretrecipe.model.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
 import java.util.HashMap;
+import java.util.List;
 
 /**
  *
@@ -16,6 +21,10 @@ import java.util.HashMap;
 public class StartController implements ControlledScreens {
 
     private HashMap<String, Parent> screens = new HashMap<>();
+    private HashMap<AnchorPane, Recipe> anchorPaneToRecipe = new HashMap<>();
+
+    private Session session;
+    private User currentUser;
 
     @FXML
     public GridPane gridPane;
@@ -25,6 +34,25 @@ public class StartController implements ControlledScreens {
 
     @FXML
     private Button newButton;
+
+    @FXML
+    void initialize() {
+        session = session.getInstance();
+
+        session.hasLoggedInProperty().addListener((observable, oldvalue, newValue) -> {
+            if (newValue) {
+                currentUser = session.getLoggedInUser();
+                List<Recipe> userRecipes = currentUser.getRecipeListe();
+
+                int gridColumnCount = gridPane.getColumnCount();
+                for (int index = 0; index < userRecipes.size(); index++) {
+                    gridPane.add(createAnchorPaneRecipeElement(userRecipes.get(index)),
+                            getColumnOfRecipe(index, gridColumnCount),
+                            getRowOfRecipe(index, gridColumnCount));
+                }
+            }
+        });
+    }
 
     /**
      * @param event
